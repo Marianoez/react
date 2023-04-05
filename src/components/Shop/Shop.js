d771fde908613e2a365ee3c61c7017f00d31eb5d
Marianoez <marianoezequielvara@gmail.com>
2023-04-04 21:07:56 -0300

    ModifyPruebas1

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,17 +2,12 @@ import React from "react";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
 const Shop = () => {
-    return ( <
-        div style = {
-            { display: "flex" } } >
-        <
-        AddShoppingCartIcon sx = {
-            { color: "white" } }
-        />{" "} <
-        span style = {
-            { color: "white" } } > Carrito < /span>{" "} <
-        /div>
-    );
+  return (
+    <div style={{ display: "flex" }}>
+      <AddShoppingCartIcon sx={{ color: "white" }} />{" "}
+      <span style={{ color: "white" }}> Carrito </span>{" "}
+    </div>
+  );
 };
 
 export default Shop;
